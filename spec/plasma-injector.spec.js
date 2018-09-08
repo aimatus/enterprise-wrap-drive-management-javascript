315148c461fcbc16c433a61c17b82234104ddca7
Abraham Matus <aimatusco@gmail.com>
2018-09-08 10:58:48 -0500

    Refactors code base to get rid of unnecessary methods.

--- spec/plasma-injector.spec.js
+++ spec/plasma-injector.spec.js
@@ -4,12 +4,6 @@ const PlasmaInjector = require('../src/plasma-injector.js');
 
 describe('PlasmaInjector', function() {
   
-  it('should return proper initialized damage value', function() {
-    const expectedDamagePercentage = 20;
-    const plasmaInjector = new PlasmaInjector(expectedDamagePercentage);
-    expect(plasmaInjector.getDamagePercentage()).to.equal(expectedDamagePercentage);
-  });
-
   it('should return proper max undefined plasma flow', function() {
     const damagePercentage = 20;
     const expectedMaxUndefinedPlasmaFlow = 80;
@@ -17,28 +11,28 @@ describe('PlasmaInjector', function() {
     expect(plasmaInjector.getMaxUndefinedPlasmaFlow()).to.equal(expectedMaxUndefinedPlasmaFlow);
   });
 
-  it('should return infinite remaining working time in minutes when no extra plasma flow is applied', function() {
+  it('should return infinite remaining working time in minutes when no extra plasma flow is provided', function() {
     const damagePercentage = 0;
     const plasmaInjector = new PlasmaInjector(damagePercentage);
     expect(plasmaInjector.getRemainingWorkingTimeInMinutes()).to.equal('infinite');
   });
 
-  it('should return proper remaining travel working time in minutes when extra plasma flow is applied', function() {
+  it('should return proper remaining travel working time in minutes when extra plasma flow is provided', function() {
     const damagePercentage = 0;
     const desiredExtraPlasmaFlow = 50;
     const expectedRemainingWorkingTimeInMinutes = 50;
     const plasmaInjector = new PlasmaInjector(damagePercentage);
-    plasmaInjector.setDesiredPlasmaFlow(desiredExtraPlasmaFlow);
-    expect(plasmaInjector.getRemainingWorkingTimeInMinutes()).to.equal(expectedRemainingWorkingTimeInMinutes);
+    const remainingWorkingTimeInMinutes = plasmaInjector.getRemainingWorkingTimeInMinutes(desiredExtraPlasmaFlow);
+    expect(remainingWorkingTimeInMinutes).to.equal(expectedRemainingWorkingTimeInMinutes);
   });
 
-  it('should return proper remaining travel working time in minutes when extra plasma flow is applied and has some damage', function() {
+  it('should return proper remaining travel working time in minutes when extra plasma flow is provided and has some damage', function() {
     const damagePercentage = 20;
     const desiredExtraPlasmaFlow = 50;
     const expectedRemainingWorkingTimeInMinutes = 30;
     const plasmaInjector = new PlasmaInjector(damagePercentage);
-    plasmaInjector.setDesiredPlasmaFlow(desiredExtraPlasmaFlow);
-    expect(plasmaInjector.getRemainingWorkingTimeInMinutes()).to.equal(expectedRemainingWorkingTimeInMinutes);
+    const remainingWorkingTimeInMinutes = plasmaInjector.getRemainingWorkingTimeInMinutes(desiredExtraPlasmaFlow);
+    expect(remainingWorkingTimeInMinutes).to.equal(expectedRemainingWorkingTimeInMinutes);
   });
 
 });
--- src/plasma-injector.js
+++ src/plasma-injector.js
@@ -1,27 +1,18 @@
 class PlasmaPropeller {
     
     constructor(damage) {
-        this.desiredExtraPlasmaFlow = 0;
         this.damagePercentage = damage;
     }
     
-    getDamagePercentage() {
-        return this.damagePercentage;
-    }
-
     getMaxUndefinedPlasmaFlow() {
         const maxUndefinedPlasmaFlow = 100;
         return maxUndefinedPlasmaFlow - this.damagePercentage;
     }
 
-    getRemainingWorkingTimeInMinutes() {
-        const hasNoExtraPlasmaFlow = this.desiredExtraPlasmaFlow === 0;
-        const remainingWorkingTimeInMinutes = this.getMaxUndefinedPlasmaFlow() - this.desiredExtraPlasmaFlow;
+    getRemainingWorkingTimeInMinutes(desiredExtraPlasmaFlow) {
+        const hasNoExtraPlasmaFlow = desiredExtraPlasmaFlow === undefined;
+        const remainingWorkingTimeInMinutes = this.getMaxUndefinedPlasmaFlow() - desiredExtraPlasmaFlow;
         return hasNoExtraPlasmaFlow ? 'infinite' : remainingWorkingTimeInMinutes;
-    }
-
-    setDesiredPlasmaFlow (desiredExtraPlasmaFlow) {
-        this.desiredExtraPlasmaFlow = desiredExtraPlasmaFlow;
     }
 
 }
