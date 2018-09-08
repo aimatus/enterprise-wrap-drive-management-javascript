f1ac801dd8688626f7a2ce2eaa300cf10986ec7e
Abraham Matus <aimatusco@gmail.com>
2018-09-08 16:04:31 -0500

    Adds colors to console app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const PlasmaReactor = require('./src/plasma-reactor');
 const PlasmaInjector = require('./src/plasma-injector');
+const colors = require('colors');
 
 const calculatePlasmaReactorOptimalSettings = () => {
     const plasmaInjectors = [
@@ -10,14 +11,15 @@ const calculatePlasmaReactorOptimalSettings = () => {
     const plasmaReactor = new PlasmaReactor(plasmaInjectors);
     const plasmaInjectorsFlow = plasmaReactor.calculateInjectorsPlasmaFlow(process.argv[5]);
     const remainingTime = plasmaReactor.getRemainingTravelTime(plasmaInjectorsFlow);
-    console.log('\n');
-    console.log('|----------------------------------------------------|');
-    console.log('|---------- ENTERPRISE WRAP DRIVE MANAGEMENT --------|');
-    console.log('|----------------------------------------------------|');
+    console.log('|----------------------------------------------------|'.green);
+    console.log('|---------- '.green + 'ENTERPRISE WRAP DRIVE MANAGEMENT'.red + ' --------|'.green);
+    console.log('|----------------------------------------------------|'.green);
+    console.log('|  '.green + 'Plasma Injectors (A, B, C)'.yellow + '                        |'.green);
+    console.log('|----------------------------------------------------|'.green);
     console.log('|    A: ' + plasmaInjectorsFlow[0] + 'mg/s', '   |    B: ' + plasmaInjectorsFlow[1] + 'mg/s', '  |   C: ' + plasmaInjectorsFlow[2] + 'mg/s');
-    console.log('|----------------------------------------------------|');
-    console.log('| Remaining working time: ' + remainingTime);
-    console.log('|----------------------------------------------------|');
+    console.log('|----------------------------------------------------|'.green);
+    console.log('| Remaining working time: '.blue + remainingTime);
+    console.log('|----------------------------------------------------|'.green);
 }
 
 calculatePlasmaReactorOptimalSettings();
